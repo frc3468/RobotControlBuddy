99411dc15e1646b160adcbff34777455aabc46e7
Gustave Michel III <gixxygamma@gmail.com>
2021-02-07 18:24:27 -0600

    Updated README, Added Code

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Robot-Control-Buddy
-Arduino Pro Micro Driven Controller to Enable/Disable/E-Stop FRC Robotics through the FRC Driverstation
+"Controller" (Emulates Keyboard) to send Enable/Disable/E-Stop commands FRC Robotics through the FRC Driverstation through keyboard shortcuts
+
+For ATmega32u4 based Microcontrollers (Arduino Leonardo, Micro, Pro Micro, ect.)
+
+Monitors inputs and sends FRC Driverstation Keyboard Shortcuts when inputs go low
+
+The circuit:
+  * Buttons/Switches attached to pins for E-Stop, Disable, and Enable
+    * E-STOP - Pin 9
+    * DISABLE - Pin 7
+    * ENABLE - Pin 6
+  * Other leg of Button/Switch attached to GND
+  * If Button/Switch is Normally Closed, reverse digitalRead() Logic
+    * Remove Bang(!) from digitalRead() if statements
+
+## CHANGELOG
+  * Created 07 Feb 2021
+
---
+++ Robot_Control_Buddy.ino
@@ -0,0 +1,93 @@
+/*    
+  FRC Robot Control Buddy - Send FRC Driverstation Shortcuts on Pin Input
+  Copyright (C) 2021  Gustave Michel III
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+  For ATMega32u4 based Arduinos (Leonardo, Micro, Pro Micro, ect.)
+
+  Monitors input pins and sends FRC Driverstation keyboard shortcuts when they go low.
+
+  The circuit:
+  - Buttons/Switches attached to pins 7,8,9 with Ground on the other leg
+  - If Buttons/Switches are Normally Closed, digitalRead() logic will need to be reversed
+    - remove the bangs(!) from the if statments
+
+  CHANGELOG
+    created 07 Feb 2021
+
+*/
+
+#include "Keyboard.h"
+
+const uint8_t estop_pin = 9;
+const uint8_t disable_pin = 7;
+const uint8_t enable_pin = 8;
+
+const char estop_key = ' ';
+const char disable_key = KEY_RETURN;
+const char enable_keys[] = { '[', ']', '\\' };
+
+bool estop_prev = false;
+bool disable_prev = false;
+bool enable_prev = false;
+
+void setup() {
+  // make the button pins inputs with pullups
+  pinMode(estop_pin, INPUT_PULLUP);
+  pinMode(disable_pin, INPUT_PULLUP);
+  pinMode(enable_pin, INPUT_PULLUP);
+  
+  // initialize keyboard interface
+  Keyboard.begin();
+}
+
+void loop() {
+  //TODO: Debounce? May not be needed
+  
+  if(!digitalRead(estop_pin)) {
+    Keyboard.press(estop_key);
+    estop_prev =  true;
+  } else {
+    if(estop_prev) {
+      Keyboard.release(estop_key);
+      estop_prev = false;
+    }
+  }
+
+  if(!digitalRead(disable_pin)) {
+    Keyboard.press(disable_key);
+    disable_prev =  true;
+  } else {
+    if(disable_prev) {
+      Keyboard.release(disable_key);
+      disable_prev = false;
+    }
+  }
+
+  if(!digitalRead(enable_pin)) {
+    for(int i = 0; i < 3; i++) {
+      Keyboard.press(enable_keys[i]);
+    }
+    enable_prev = true;
+  } else {
+    if(enable_prev) {
+      for(int i = 0; i < 3; i++) {
+        Keyboard.release(enable_keys[i]);
+      }
+      enable_prev = false;
+    }
+  }
+}
